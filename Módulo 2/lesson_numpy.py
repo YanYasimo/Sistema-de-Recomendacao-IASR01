591c9ee6ccc879043765c4012fd73592c429e3ea
Yan Moura <yanmoura142529@gmail.com>
2023-09-18 22:53:15 -0300

    Aula numpy

---
+++ Módulo 2/lesson_numpy.py
@@ -0,0 +1,19 @@
+#%%
+
+import numpy as np 
+data = np.array([1,2,3,4,5])
+data
+
+# %%
+matriz = np.array([[1,2,3],[4,5,6],[7,8,9]])
+matriz
+
+# %%
+matriz = np.zeros((3,3))
+matriz
+
+# %%
+
+identidade = np.eye(7)
+identidade
+# %%
