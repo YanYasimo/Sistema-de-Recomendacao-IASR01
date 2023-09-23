073c59f9182462822fc7ce65c672869246f6d63a
Yan Moura <yanmoura142529@gmail.com>
2023-09-23 15:31:47 -0300

    Create async.py

---
+++ Módulo 3/async.py
@@ -0,0 +1,35 @@
+import asyncio
+import numpy as np
+import time
+from sklearn.linear_model import LinearRegression
+
+def generate_data(n_samples = 100):
+    # Generate random data
+    X = np.random.rand(n_samples, 1)
+    y = 2.5 * X + np.random.rand(n_samples, 1)
+    return X, y
+
+async def fetch_data(delay = 2):
+    # Generate data
+    await asyncio.sleep(delay) # Simulate network delay
+    data = generate_data()
+    return data
+
+async def train_model_async(delay = 3):
+    # Train model
+    print(f"Training model...")
+    X, y = await fetch_data() # Wait for data to be fetched
+    await asyncio.sleep(delay) # Simulate training time
+    model = LinearRegression()
+    model.fit(X, y)
+    print(f"Model trained!")
+
+# executing async tasks 
+async def main():
+    # Different delays for each task
+    tasks = [
+        train_model_async(i) for i in [2, 3, 4]
+    ]
+
+    # Wait for all tasks to finish
+    await asyncio.gather(*tasks)
